58db55a64dce0caefa58fbe578f0670386dfd28d
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-23 01:54:21 +0300

    Update ReviewController.cs

--- Pokemon/Controllers/ReviewController.cs
+++ Pokemon/Controllers/ReviewController.cs
@@ -96,7 +96,7 @@ namespace PokemonReviewApp.Controllers
 
             if (!_reviewRepository.CreateReview(reviewMap))
             {
-                ModelState.AddModelError("", "Something went wrong while savin");
+                ModelState.AddModelError("", "Something went wrong while saving");
                 return StatusCode(500, ModelState);
             }
 
