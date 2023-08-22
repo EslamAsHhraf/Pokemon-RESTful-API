02515cba6fc8c9de872af2226dc755686a2a30af
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-22 11:30:03 +0300

    Update CategoryController.cs

--- Pokemon/Controllers/CategoryController.cs
+++ Pokemon/Controllers/CategoryController.cs
@@ -86,7 +86,7 @@ namespace PokemonReviewApp.Controllers
 
             if (!_categoryRepository.CreateCategory(categoryMap))
             {
-                ModelState.AddModelError("", "Something went wrong while savin");
+                ModelState.AddModelError("", "Something went wrong while saving");
                 return StatusCode(500, ModelState);
             }
 
